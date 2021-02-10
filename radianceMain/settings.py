4e3e2be3ed62bb0f37a06c14dedbc4a20c772caf
Chahat Kalsi <chahat.ck88@gmail.com>
2021-02-10 11:45:20 +0530

    secret key hidden

--- radianceMain/settings.py
+++ radianceMain/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a*65th1t(jjgg6@8hea01%^zoe&w*(&qi5cu4&(qtwk@@zjsb!'
+SECRET_KEY = os.getenv(DJANGO_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
