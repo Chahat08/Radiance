7aa62d10029b7e640cdc0277cd746bed1a005b91
Chahat Kalsi <chahat.ck88@gmail.com>
2021-02-12 22:46:20 +0530

    pythonanywhere added to allowed hosts

--- radianceMain/settings.py
+++ radianceMain/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
